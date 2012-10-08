564ef39bcd0c6d21a59bfc6d89c58b377abcd756
Nripendra nath newa <nripendra@uba-solutions.com>
2012-10-08 18:55:44 +0545

    Moved away License.txt into it's own folder.
    other Nuget specific changes.

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Resty.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple REST client for .net framework. Serves as a very thin layer above HttpWebRequest object exposes most of its properties. Also provides an option to directly access the underlying HttpWebRequest and HttpWebResponse objects if necessary.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nripendra Nath Newa")]
 [assembly: AssemblyProduct("Resty.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.1-beta")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
