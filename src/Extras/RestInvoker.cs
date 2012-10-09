68338a988beb71674e678cd776c5cb76155c9799
Nripendra nath newa <nripendra@uba-solutions.com>
2012-10-09 16:47:26 +0545

    Start RestInvoker modifications.

--- src/Extras/RestInvoker.cs
+++ src/Extras/RestInvoker.cs
@@ -23,8 +23,21 @@ using System.Threading.Tasks;
 
 namespace Resty.Net.Extras
 {
+    using Extensions;
+
     public class RestInvoker
     {
+        private Uri _baseUrl;
+
+        public RestInvoker()
+        {
+        }
+
+        public RestInvoker(string baseUrl)
+        {
+            _baseUrl = new Uri(baseUrl, UriKind.Absolute);
+        }
+
         public Task<RestResponse> InvokeAsync(RestRequest request)
         {
             return request.GetResponseAsync();
@@ -50,39 +63,188 @@ namespace Resty.Net.Extras
             return InvokeAsync(CreateRestRequest(HttpMethod.GET, uri));
         }
 
+        public Task<RestResponse> GetAsync(string resourceUri)
+        {
+            return GetAsync(CreateRestUri(_baseUrl, resourceUri, new { }));
+        }
+
+        public Task<RestResponse> GetAsync(string resourceUri, object parameters)
+        {
+            return GetAsync(CreateRestUri(_baseUrl, resourceUri, parameters));
+        }
+
+        public RestResponse Get(string resourceUri)
+        {
+            return GetAsync(resourceUri).Result;
+        }
+
+        public RestResponse Get(string resourceUri, object parameters)
+        {
+            return GetAsync(resourceUri, parameters).Result;
+        }
+
         public RestResponse Get(RestUri uri)
         {
-            return Invoke(CreateRestRequest(HttpMethod.GET, uri));
+            return GetAsync(uri).Result;
+        }
+
+        public Task<RestResponse> PostAsync(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            RestRequest request = CreateRestRequest(HttpMethod.POST, uri);
+            request.ContentType = contentType;
+            request.Body = requestBody;
+            return InvokeAsync(request);
         }
 
         public Task<RestResponse> PostAsync(RestUri uri, RestRequestBody requestBody)
         {
-            return InvokeAsync(CreateRestRequest(HttpMethod.POST, uri));
+            return PostAsync(uri, requestBody, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse> PostAsync(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PostAsync(CreateRestUri(_baseUrl, resourceUri, parameters), body, contentType);
+        }
+
+        public Task<RestResponse> PostAsync(string resourceUri, RestRequestBody body)
+        {
+            return PostAsync(CreateRestUri(_baseUrl, resourceUri, new { }), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse> PostAsync(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PostAsync(CreateRestUri(_baseUrl, resourceUri, parameters), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public RestResponse Post(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            return PostAsync(uri, requestBody, contentType).Result;
         }
 
         public RestResponse Post(RestUri uri, RestRequestBody requestBody)
         {
-            return Invoke(CreateRestRequest(HttpMethod.POST, uri));
+            return PostAsync(uri, requestBody).Result;
+        }
+
+        public RestResponse Post(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PostAsync(resourceUri, parameters, body, contentType).Result;
+        }
+
+        public RestResponse Post(string resourceUri, RestRequestBody body)
+        {
+            return PostAsync(resourceUri, body).Result;
+        }
+
+        public RestResponse Post(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PostAsync(resourceUri, parameters, body).Result;
+        }
+
+        public Task<RestResponse> PutAsync(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            RestRequest request = CreateRestRequest(HttpMethod.PUT, uri);
+            request.Body = requestBody;
+            request.ContentType = contentType;
+            return InvokeAsync(request);
         }
 
         public Task<RestResponse> PutAsync(RestUri uri, RestRequestBody requestBody)
         {
-            return InvokeAsync(CreateRestRequest(HttpMethod.PUT, uri));
+            return PutAsync(uri, requestBody, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse> PutAsync(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PutAsync(CreateRestUri(_baseUrl, resourceUri, parameters), body, contentType);
+        }
+
+        public Task<RestResponse> PutAsync(string resourceUri, RestRequestBody body)
+        {
+            return PutAsync(CreateRestUri(_baseUrl, resourceUri, new { }), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse> PutAsync(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PutAsync(CreateRestUri(_baseUrl, resourceUri, parameters), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public RestResponse Put(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            return PutAsync(uri, requestBody, contentType).Result;
         }
 
         public RestResponse Put(RestUri uri, RestRequestBody requestBody)
         {
-            return Invoke(CreateRestRequest(HttpMethod.PUT, uri));
+            return PutAsync(uri, requestBody).Result;
+        }
+
+        public RestResponse Put(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PutAsync(resourceUri, parameters, body, contentType).Result;
+        }
+
+        public RestResponse Put(string resourceUri, RestRequestBody body)
+        {
+            return PutAsync(resourceUri, body).Result;
+        }
+
+        public RestResponse Put(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PutAsync(resourceUri, parameters, body).Result;
+        }
+
+        public Task<RestResponse> PatchAsync(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            RestRequest request = CreateRestRequest(HttpMethod.PATCH, uri);
+            request.Body = requestBody;
+            request.ContentType = contentType;
+            return InvokeAsync(request);
         }
 
         public Task<RestResponse> PatchAsync(RestUri uri, RestRequestBody requestBody)
         {
-            return InvokeAsync(CreateRestRequest(HttpMethod.PATCH, uri));
+            return PatchAsync(uri, requestBody, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse> PatchAsync(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PatchAsync(CreateRestUri(_baseUrl, resourceUri, parameters), body, contentType);
+        }
+
+        public Task<RestResponse> PatchAsync(string resourceUri, RestRequestBody body)
+        {
+            return PatchAsync(CreateRestUri(_baseUrl, resourceUri, new { }), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse> PatchAsync(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PatchAsync(CreateRestUri(_baseUrl, resourceUri, parameters), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public RestResponse Patch(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            return PatchAsync(uri, requestBody, contentType).Result;
         }
 
         public RestResponse Patch(RestUri uri, RestRequestBody requestBody)
         {
-            return Invoke(CreateRestRequest(HttpMethod.PATCH, uri));
+            return PatchAsync(uri, requestBody).Result;
+        }
+
+        public RestResponse Patch(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PatchAsync(resourceUri, parameters, body, contentType).Result;
+        }
+
+        public RestResponse Patch(string resourceUri, RestRequestBody body)
+        {
+            return PatchAsync(resourceUri, body).Result;
+        }
+
+        public RestResponse Patch(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PatchAsync(resourceUri, parameters, body).Result;
         }
 
         public Task<RestResponse> DeleteAsync(RestUri uri)
@@ -90,9 +252,29 @@ namespace Resty.Net.Extras
             return InvokeAsync(CreateRestRequest(HttpMethod.DELETE, uri));
         }
 
+        public Task<RestResponse> DeleteAsync(string resourceUri)
+        {
+            return DeleteAsync(new RestUri(_baseUrl, resourceUri));
+        }
+
+        public Task<RestResponse> DeleteAsync(string resourceUri, object parameters)
+        {
+            return DeleteAsync(CreateRestUri(_baseUrl, resourceUri, parameters));
+        }
+
+        public RestResponse Delete(string resourceUri)
+        {
+            return DeleteAsync(resourceUri).Result;
+        }
+
+        public RestResponse Delete(string resourceUri, object parameters)
+        {
+            return DeleteAsync(resourceUri, parameters).Result;
+        }
+
         public RestResponse Delete(RestUri uri)
         {
-            return Invoke(CreateRestRequest(HttpMethod.DELETE, uri));
+            return DeleteAsync(uri).Result;
         }
 
         public Task<RestResponse<T>> GetAsync<T>(RestUri uri)
@@ -100,39 +282,188 @@ namespace Resty.Net.Extras
             return InvokeAsync<T>(CreateRestRequest(HttpMethod.GET, uri));
         }
 
+        public Task<RestResponse<T>> GetAsync<T>(string resourceUri)
+        {
+            return GetAsync<T>(CreateRestUri(_baseUrl, resourceUri, new { }));
+        }
+
+        public Task<RestResponse<T>> GetAsync<T>(string resourceUri, object parameters)
+        {
+            return GetAsync<T>(CreateRestUri(_baseUrl, resourceUri, parameters));
+        }
+
+        public RestResponse<T> Get<T>(string resourceUri)
+        {
+            return GetAsync<T>(resourceUri).Result;
+        }
+
+        public RestResponse<T> Get<T>(string resourceUri, object parameters)
+        {
+            return GetAsync<T>(resourceUri, parameters).Result;
+        }
+
         public RestResponse<T> Get<T>(RestUri uri)
         {
-            return Invoke<T>(CreateRestRequest(HttpMethod.GET, uri));
+            return GetAsync<T>(uri).Result;
+        }
+
+        public Task<RestResponse<T>> PostAsync<T>(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            RestRequest request = CreateRestRequest(HttpMethod.POST, uri);
+            request.ContentType = contentType;
+            request.Body = requestBody;
+            return InvokeAsync<T>(request);
         }
 
         public Task<RestResponse<T>> PostAsync<T>(RestUri uri, RestRequestBody requestBody)
         {
-            return InvokeAsync<T>(CreateRestRequest(HttpMethod.POST, uri));
+            return PostAsync<T>(uri, requestBody, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse<T>> PostAsync<T>(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PostAsync<T>(CreateRestUri(_baseUrl, resourceUri, parameters), body, contentType);
+        }
+
+        public Task<RestResponse<T>> PostAsync<T>(string resourceUri, RestRequestBody body)
+        {
+            return PostAsync<T>(CreateRestUri(_baseUrl, resourceUri, new { }), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse<T>> PostAsync<T>(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PostAsync<T>(CreateRestUri(_baseUrl, resourceUri, parameters), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public RestResponse<T> Post<T>(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            return PostAsync<T>(uri, requestBody, contentType).Result;
         }
 
         public RestResponse<T> Post<T>(RestUri uri, RestRequestBody requestBody)
         {
-            return Invoke<T>(CreateRestRequest(HttpMethod.POST, uri));
+            return PostAsync<T>(uri, requestBody).Result;
+        }
+
+        public RestResponse<T> Post<T>(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PostAsync<T>(resourceUri, parameters, body, contentType).Result;
+        }
+
+        public RestResponse<T> Post<T>(string resourceUri, RestRequestBody body)
+        {
+            return PostAsync<T>(resourceUri, body).Result;
+        }
+
+        public RestResponse<T> Post<T>(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PostAsync<T>(resourceUri, parameters, body).Result;
+        }
+
+        public Task<RestResponse<T>> PutAsync<T>(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            RestRequest request = CreateRestRequest(HttpMethod.PUT, uri);
+            request.Body = requestBody;
+            request.ContentType = contentType;
+            return InvokeAsync<T>(request);
         }
 
         public Task<RestResponse<T>> PutAsync<T>(RestUri uri, RestRequestBody requestBody)
         {
-            return InvokeAsync<T>(CreateRestRequest(HttpMethod.PUT, uri));
+            return PutAsync<T>(uri, requestBody, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse<T>> PutAsync<T>(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PutAsync<T>(CreateRestUri(_baseUrl, resourceUri, parameters), body, contentType);
+        }
+
+        public Task<RestResponse<T>> PutAsync<T>(string resourceUri, RestRequestBody body)
+        {
+            return PutAsync<T>(CreateRestUri(_baseUrl, resourceUri, new { }), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse<T>> PutAsync<T>(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PutAsync<T>(CreateRestUri(_baseUrl, resourceUri, parameters), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public RestResponse<T> Put<T>(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            return PutAsync<T>(uri, requestBody, contentType).Result;
         }
 
         public RestResponse<T> Put<T>(RestUri uri, RestRequestBody requestBody)
         {
-            return Invoke<T>(CreateRestRequest(HttpMethod.PUT, uri));
+            return PutAsync<T>(uri, requestBody).Result;
+        }
+
+        public RestResponse<T> Put<T>(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PutAsync<T>(resourceUri, parameters, body, contentType).Result;
+        }
+
+        public RestResponse<T> Put<T>(string resourceUri, RestRequestBody body)
+        {
+            return PutAsync<T>(resourceUri, body).Result;
+        }
+
+        public RestResponse<T> Put<T>(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PutAsync<T>(resourceUri, parameters, body).Result;
+        }
+
+        public Task<RestResponse<T>> PatchAsync<T>(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            RestRequest request = CreateRestRequest(HttpMethod.PATCH, uri);
+            request.Body = requestBody;
+            request.ContentType = contentType;
+            return InvokeAsync<T>(request);
         }
 
         public Task<RestResponse<T>> PatchAsync<T>(RestUri uri, RestRequestBody requestBody)
         {
-            return InvokeAsync<T>(CreateRestRequest(HttpMethod.PATCH, uri));
+            return PatchAsync<T>(uri, requestBody, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse<T>> PatchAsync<T>(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PatchAsync<T>(CreateRestUri(_baseUrl, resourceUri, parameters), body, contentType);
+        }
+
+        public Task<RestResponse<T>> PatchAsync<T>(string resourceUri, RestRequestBody body)
+        {
+            return PatchAsync<T>(CreateRestUri(_baseUrl, resourceUri, new { }), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public Task<RestResponse<T>> PatchAsync<T>(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PatchAsync<T>(CreateRestUri(_baseUrl, resourceUri, parameters), body, ContentType.ApplicationX_WWW_Form_UrlEncoded);
+        }
+
+        public RestResponse<T> Patch<T>(RestUri uri, RestRequestBody requestBody, ContentType contentType)
+        {
+            return PatchAsync<T>(uri, requestBody, contentType).Result;
         }
 
         public RestResponse<T> Patch<T>(RestUri uri, RestRequestBody requestBody)
         {
-            return Invoke<T>(CreateRestRequest(HttpMethod.PATCH, uri));
+            return PatchAsync<T>(uri, requestBody).Result;
+        }
+
+        public RestResponse<T> Patch<T>(string resourceUri, object parameters, RestRequestBody body, ContentType contentType)
+        {
+            return PatchAsync<T>(resourceUri, parameters, body, contentType).Result;
+        }
+
+        public RestResponse<T> Patch<T>(string resourceUri, RestRequestBody body)
+        {
+            return PatchAsync<T>(resourceUri, body).Result;
+        }
+
+        public RestResponse<T> Patch<T>(string resourceUri, object parameters, RestRequestBody body)
+        {
+            return PatchAsync<T>(resourceUri, parameters, body).Result;
         }
 
         public Task<RestResponse<T>> DeleteAsync<T>(RestUri uri)
@@ -140,14 +471,55 @@ namespace Resty.Net.Extras
             return InvokeAsync<T>(CreateRestRequest(HttpMethod.DELETE, uri));
         }
 
+        public Task<RestResponse<T>> DeleteAsync<T>(string resourceUri)
+        {
+            return DeleteAsync<T>(new RestUri(_baseUrl, resourceUri));
+        }
+
+        public Task<RestResponse<T>> DeleteAsync<T>(string resourceUri, object parameters)
+        {
+            return DeleteAsync<T>(CreateRestUri(_baseUrl, resourceUri, parameters));
+        }
+
+        public RestResponse<T> Delete<T>(string resourceUri)
+        {
+            return DeleteAsync<T>(resourceUri).Result;
+        }
+
+        public RestResponse<T> Delete<T>(string resourceUri, object parameters)
+        {
+            return DeleteAsync<T>(resourceUri, parameters).Result;
+        }
+
         public RestResponse<T> Delete<T>(RestUri uri)
         {
-            return Invoke<T>(CreateRestRequest(HttpMethod.DELETE, uri));
+            return DeleteAsync<T>(uri).Result;
         }
 
         private RestRequest CreateRestRequest(HttpMethod method, RestUri uri)
         {
             return new RestRequest(method, uri);
+        }
+
+        private RestUri CreateRestUri(Uri baseUri, string resourceUri, object parameters)
+        {
+            var uri = new RestUri(baseUri, resourceUri);
+
+            var nameValuePairs = parameters.ToDictionary();
+
+            foreach (var nameValue in nameValuePairs)
+            {
+                if (resourceUri.IndexOf("{" + nameValue.Key + "}") > -1)
+                {
+                    uri.SetParameter(nameValue.Value);
+                }
+                else
+                {
+                    uri.SetQuery(nameValue.Value);
+                }
+            }
+
+            return uri;
         }
 
     }
